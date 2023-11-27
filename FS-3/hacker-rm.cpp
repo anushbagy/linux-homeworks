ee7d2283587b246a0c7a5ce4ee81ac2b2fbd092b
anushbagy <anush.bagdasaryan2004@gmail.com>
2023-11-27 17:41:17 +0400

    TH-1

--- FS-3/hacker-rm.cpp
+++ FS-3/hacker-rm.cpp
@@ -66,3 +66,63 @@
     
     return 0;
  }
+//  #include <iostream>
+// #include <sys/types.h>
+// #include <unistd.h>
+// #include <cerrno>
+// #include <sys/wait.h>
+// #include <bits/stdc++.h> 
+// #include <ctime>
+
+// using namespace std; 
+
+//  void do_command(char** argv){
+
+// // start time of the program
+// struct tms start, end; 
+// clock_t beginTime,endTime;
+// beginTime = times(&start);
+//   // std::time_t beginTime = std::time(nullptr);
+//   // std::time_t endTime;
+  
+// //making new process
+//   pid_t pidForprocess = fork();
+
+// //check if it creates correct
+//   if(pidForprocess == -1){
+//    std::cerr << strerror(errno) << std::endl;
+//    exit(errno);
+//   }
+//  int second_process = execv(argv[0],argv);
+//  if(second_process == -1){
+//    std::cerr << strerror(errno) << std::endl;
+//    exit(errno);
+//   }
+//    int pidStatus = -1;
+
+//    pid_t waitingPid = waitpid(pidForprocess,&pidStatus,0); 
+//    if(waitingPid == -1){
+//    std::cerr << strerror(errno) << std::endl;
+//    exit(errno);
+//    }
+
+//    endTime = times(&end);
+//      if (WIFEXITED(pidStatus)) {
+//      double total_time = difftime(endTime,beginTime)/_SC_;
+//     std::cout<< "Done with "<< WEXITSTATUS(pidStatus) <<" "<<"time"<< total_time <<std::endl;
+//   }
+
+//  }
+
+//  }
+// int main(int argc, char* argv[]){
+
+// if(argc < 2){
+//    std::cerr << "no path" << std::endl;
+//    exit(1);
+// }
+// do_command(argv);
+
+// return 0;
+ 
+// }
